19993de4cf5300fa766dbc8a1ce1e919aadc1a7e
AliAmmar <aliammarali125@gmail.com>
2025-01-05 00:43:34 +0300

    add build app command

--- app/lib/src/core/provider/dio.dart
+++ app/lib/src/core/provider/dio.dart
@@ -1,8 +1,6 @@
-import 'dart:io';
 import 'package:app/src/core/provider/config.dart';
 import 'package:app/src/core/utils/log.dart';
 import 'package:dio/dio.dart';
-import 'package:dio/io.dart';
 
 import 'package:riverpod/riverpod.dart';
 import 'package:talker_dio_logger/talker_dio_logger.dart';
@@ -11,13 +9,6 @@ final dioClient = Provider((ref) {
   final dio = Dio(BaseOptions(
     baseUrl: ref.read(configProvider).baseUrl,
   ));
-  // allow self-signed certificate
-  //TODO: check if needed in producation
-  (dio.httpClientAdapter as IOHttpClientAdapter).createHttpClient = () {
-    final client = HttpClient();
-    client.badCertificateCallback = (cert, host, port) => true;
-    return client;
-  };
 
   // dio.interceptors.add(TokenInterceptors(dio, () {
   //   return getToken(ref);
